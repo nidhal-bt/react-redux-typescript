6844e0901429edd945465bb82e59f4c7dfaccd10
nidhal-bt <bentaaritn@gmail.com>
2024-11-06 15:50:58 +0100

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { RouterProvider } from "react-router-dom";
 import AppRouter from "./routes/app-routes";
 
 function App() {
+  console.log();
   return <RouterProvider router={AppRouter} />;
 }
 
